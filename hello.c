097b1400f29b020642baeb4f13af228499401819
Zach Main <zmain2@huskers.unl.edu>
2020-08-18 01:49:29 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Zach Main
+ * Date: 8/17/20
+ * 
+ * A simple hello world progam
+ **/
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv){
+     printf("Hello World\n");
+     
+     return 0;
+ }

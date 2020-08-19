e1d4a5af7fe375a68fe12bd33d00a9dc7d27e014
zmain2 <69825575+zmain2@users.noreply.github.com>
2020-08-19 18:22:40 -0500

    Add files via upload

---
+++ hello (1).c
@@ -0,0 +1,17 @@
+/**
+ * Author: Zach Main
+ * Date: 8/17/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zach Main\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
